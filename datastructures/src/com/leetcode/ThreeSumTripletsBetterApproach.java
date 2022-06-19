31cae2d8b1d46b3dd8934f279f35ca097b69018f
Hari <javahari311@gmail.com>
2022-06-19 22:06:50 +0530

    Three sum triplet Better time complexity solution

--- datastructures/src/com/leetcode/ThreeSumTripletsBetterApproach.java
+++ datastructures/src/com/leetcode/ThreeSumTripletsBetterApproach.java
@@ -21,7 +21,7 @@ public class ThreeSumTripletsBetterApproach {
 
         Arrays.sort(nums);
 
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length-2;i++){
 
             int requiredValue = -(nums[i]);
 
