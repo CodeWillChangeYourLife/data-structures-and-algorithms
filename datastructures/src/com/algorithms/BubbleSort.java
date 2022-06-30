92078ac88a9404f98ad7275dacc2584283c0f7ce
Hari <javahari311@gmail.com>
2022-07-01 00:01:00 +0530

    bubble sort algorithm

---
+++ datastructures/src/com/algorithms/BubbleSort.java
@@ -0,0 +1,23 @@
+package com.algorithms;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] arr = {10, -1, 2, 3, 4};
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
