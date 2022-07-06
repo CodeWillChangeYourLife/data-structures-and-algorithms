d990c7f10a246dd824ea25b0bd29cb8389a19b36
Hari <javahari311@gmail.com>
2022-07-06 09:33:55 +0530

    intro to 2d array

---
+++ datastructures/src/com/twodimensionalarrays/TwoDimensionalArray.java
@@ -0,0 +1,27 @@
+package com.twodimensionalarrays;
+
+public class TwoDimensionalArray {
+
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][4];
+
+        int no = 10;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = no;
+                no = no + 10;
+            }
+        }
+
+        // display twod array data
+
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+                System.out.print(arr[row][col]+" ");
+            }
+
+            System.out.println();
+        }
+    }
+    }
