6f156b83c6eb7386a002663128f04ec47cea546a
Hari <javahari311@gmail.com>
2022-07-07 21:36:27 +0530

    SumOfLowerTriangleOfBelowDiagn

---
+++ datastructures/src/com/twodimensionalarrays/SumOfLowerTriangleOfBelowDiagn.java
@@ -0,0 +1,21 @@
+package com.twodimensionalarrays;
+
+public class SumOfLowerTriangleOfBelowDiagn {
+
+    public static void main(String[] args) {
+        int [][] arr = {{1,2,3,4},
+                        {5,6,7,8},
+                        {8,7,6,5},
+                        {4,3,2,1}};
+
+        int sum =0;
+
+        for(int i=1;i<arr.length;i++){
+            for(int j=0;j<i;j++){
+                sum = sum+arr[i][j];
+            }
+        }
+
+        System.out.println("Result of sum of lower triangle of diagonal: "+ sum);
+    }
+}
