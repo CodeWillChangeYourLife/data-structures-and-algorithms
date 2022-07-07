fef42497e58c686d8176965fcffa400b9bccd425
Hari <javahari311@gmail.com>
2022-07-07 21:36:51 +0530

    SumOfUpperTriangleInMatrix

---
+++ datastructures/src/com/twodimensionalarrays/SumOfUpperTriangleInMatrix.java
@@ -0,0 +1,21 @@
+package com.twodimensionalarrays;
+
+public class SumOfUpperTriangleInMatrix {
+
+    public static void main(String[] args) {
+        int [][] arr = {{1,2,3,4},
+                        {5,6,7,8},
+                        {8,7,6,5},
+                        {4,3,2,1}};
+        int upperTriangleSum = 0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                upperTriangleSum=upperTriangleSum+arr[i][j];
+            }
+        }
+
+        System.out.println("Upper Triangle Sum : "+upperTriangleSum);
+    }
+
+
+}
