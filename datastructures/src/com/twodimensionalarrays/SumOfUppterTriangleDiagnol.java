3256ef6b65f9fd46d1921377904c770bd8e4a9d5
Hari <javahari311@gmail.com>
2022-07-08 07:40:11 +0530

    SumOfUppterTriangleDiagnol

--- datastructures/src/com/twodimensionalarrays/SumOfUppterTriangleDiagnol.java
+++ datastructures/src/com/twodimensionalarrays/SumOfUppterTriangleDiagnol.java
@@ -3,19 +3,33 @@ package com.twodimensionalarrays;
 public class SumOfUppterTriangleDiagnol {
 
     public static void main(String[] args) {
-        int [][] arr = {{1,2,3,4},
-                        {5,6,7,8},
-                        {8,7,6,5},
-                        {4,3,2,1}};
+        int[][] arr = {{1,2},{3,4},{5,6}};
 
-        int  sum = 0;
 
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
-                sum = sum+arr[i][j];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        int[][] mt = new int[arr[1].length][arr.length];
+
+        System.out.println(arr[1].length);
+        System.out.println(arr.length);
+
+        for(int i=0;i<arr[1].length;i++){
+            for(int j=0;j<arr.length;j++){
+                mt[i][j]=arr[j][i];
             }
         }
 
-        System.out.println("Total Sum :"+sum);
+        for(int i=0;i<mt.length;i++){
+            for(int j=0;j<mt[i].length;j++){
+                System.out.print(mt[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
