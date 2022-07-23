fa0b0b7daa28eb1e55446704ac7103d1fd2b60fa
Hari <javahari311@gmail.com>
2022-07-23 16:49:55 +0530

    Added Removed Methods with valdating input index value in ArrayList Data Structures

--- datastructures/src/com/customcollection/MyArrayList.java
+++ datastructures/src/com/customcollection/MyArrayList.java
@@ -2,67 +2,95 @@ package com.customcollection;
 
 public class MyArrayList<E> {
     Object[] data;
-    int index=0;
+    int index = 0;
 
-    MyArrayList(){
-        data=new Object[10];
+    MyArrayList() {
+        data = new Object[10];
     }
 
-    public void addFirst(E e){
+    public void addFirst(E e) {
 
-        for(int i=index;i>0;i--){
-            data[i]=data[i-1];
+        for (int i = index; i > 0; i--) {
+            data[i] = data[i - 1];
         }
-        data[0]=e;
+        data[0] = e;
         index++;
     }
 
-    public void removeFirst(){
-        for(int i=0;i<index;i++){
-            data[i]=data[i+1];
+    public void removeFirst() {
+        for (int i = 0; i < index; i++) {
+            data[i] = data[i + 1];
         }
         index--;
     }
-    public void size(){
-        System.out.println(index);
+
+    public void size() {
+        System.out.println("ArrayList Size :" + index);
     }
-    void add(E e){
-        if(index==data.length){
+
+    void add(E e) {
+        if (index == data.length) {
             increaseArrayCapacity();
-            data[index]=e;
-        }else{
-           data[index]=e;
+            data[index] = e;
+        } else {
+            data[index] = e;
         }
         index++;
     }
 
-    public void display(){
-        for(int i=0;i<index;i++){
-            System.out.print(data[i]+" ");
+    public void display() {
+        for (int i = 0; i < index; i++) {
+            System.out.print(data[i] + " ");
         }
         System.out.println();
+        System.out.println("END===================");
     }
 
     private void addAt(int inputIndex, E e) {
-        for(int i=index;i>inputIndex;i--){
-            data[i]=data[i-1];
+        if (inputIndex > index || inputIndex < 0) {
+            System.out.println("Invalid Index Passed");
+            return;
         }
-        data[inputIndex]=e;
+        for (int i = index; i > inputIndex; i--) {
+            data[i] = data[i - 1];
+        }
+        data[inputIndex] = e;
         index++;
     }
 
-    public void replace(int inputIndex, E e){
-        data[inputIndex]=e;
+    public void replace(int inputIndex, E e) {
+        data[inputIndex] = e;
     }
 
 
     private void increaseArrayCapacity() {
-        int newSize=data.length*2;
+        int newSize = data.length * 2;
         Object[] newArray = new Object[newSize];
-        for(int i=0;i<data.length;i++){
-            newArray[i]=data[i];
+        for (int i = 0; i < data.length; i++) {
+            newArray[i] = data[i];
         }
         data = newArray;
+    }
+
+    public void remove() {
+
+        E e = (E) data[index - 1];
+        index--;
+        System.out.println("Removed " + e);
+    }
+
+    public void removeAt(int inputIndex) {
+        if (inputIndex > index || inputIndex < 0) {
+            System.out.println("Invalid Index Value Passed");
+            return;
+        } else {
+            E e = (E) data[inputIndex];
+            System.out.println("Removed Element At Specific Index :" + e);
+            for (int i = inputIndex; i < index; i++) {
+                data[i] = data[i + 1];
+            }
+            index--;
+        }
     }
 
     public static void main(String[] args) {
@@ -78,10 +106,10 @@ public class MyArrayList<E> {
         al.removeFirst();
         al.display();
         al.size();
-        al.addAt(1,200);
+        al.addAt(1, 200);
         al.display();
         al.size();
-        al.replace(1,300);
+        al.replace(1, 300);
         al.display();
         al.size();
 
@@ -91,7 +119,7 @@ public class MyArrayList<E> {
         al1.add("Miller");
         al1.add("Clerk");
         al1.add("Shyam");
-        
+
         al1.display();
         al1.size();
 
@@ -104,5 +132,14 @@ public class MyArrayList<E> {
 
         al2.display();
         al2.size();
+        al2.remove();
+        al2.display();
+        al2.size();
+        al2.add("My ArrayList Object");
+        al2.size();
+        al2.display();
+        al2.removeAt(2);
+        al2.size();
+        al2.display();
     }
 }
